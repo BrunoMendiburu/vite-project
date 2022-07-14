9b1600e965f239e8386e665184e602b3b3d7cb42
BrunoMendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2022-07-14 11:57:16 -0300

    Initial commit

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './nav.css'
+
+const Nav = () => {
+  return (
+        <div className="container-div">
+            <header>
+                <div className="linea">
+                    <h1>PAPUCHO GVNG</h1>
+                    <h2>Welcome to first PAPUCHO web in Latam.</h2>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Nav
